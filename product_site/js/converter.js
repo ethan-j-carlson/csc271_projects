ac159b4dd464f302b6d2b706117e1ae0393056ac
ethan-j-carlson <98480330+ethan-j-carlson@users.noreply.github.com>
2024-10-20 23:35:22 -0400

    Added functionality to converter

---
+++ product_site/js/converter.js
@@ -0,0 +1,410 @@
+//https://www.w3schools.com/js/js_json.asp
+//This dictionary was generated programmatically with Python
+var graphemes = {' ': ' ',
+    '0': '↘',
+    '1': '˩',
+    '2': '˨',
+    '3': '˧',
+    '4': '˦',
+    '5': '˥',
+    '6': '↓',
+    '7': '↑',
+    '9': '↗',
+    'A': 'ɑ',
+    'A)': 'ɒ',
+    'E': 'ɛ',
+    'E"': 'ɜ',
+    'E)': 'œ',
+    'I': 'ɪ',
+    'I"': 'ɘ',
+    'O': 'ɔ',
+    'O"': 'ɞ',
+    'O(': 'ʌ',
+    'U': 'ʊ',
+    'Y': 'ʏ',
+    '@': 'æ',
+    '*': '*',
+    '*+': '\\\\',
+    '*-': '͍',
+    '*%': '̑',
+    '*_': '͢',
+    '*=': '̻',
+    '*H': '⁼',
+    '*W': 'ʬ',
+    '*M': '\uf07d',
+    '*G': '\uf005',
+    '*K': '\uf004',
+    '*P': '¡',
+    '*>': '͔',
+    '*<': '͕',
+    '*R': 'ↀ',
+    '*"': '͈',
+    '*.': '͉',
+    '*)': '₎',
+    '*(': '₍',
+    '*B': 'ʱ',
+    '*C': '̰',
+    '*N': '\uf006',
+    '*D': '͊',
+    '*L': '\uf07c',
+    '*V': '↓',
+    '*|': '͎',
+    '*X': '\uf003',
+    '*F': 'ʩ',
+    '*~': '͌',
+    '*T': 'ʭ',
+    '*S': 'ꞎ',
+    '*Z': '\uf035',
+    '?': 'ʔ',
+    '?;': 'ʢ',
+    '?+': 'ʡ',
+    '?H': 'ʕ', "'": 'ʼ', "'+": '˖', "'-": '⁻', "'%": '̑', "';": '˕', "',": '̍', "'_": '˽', "'H": 'ʰ', "'W": 'ʷ', "'M": '̼', "'J": 'ʲ', "'G": 'ˠ', "'P": 'ˤ', "'!": '᷵', "'>": '꭪', "'<": '꭫', "'R": '˞',
+    '\'"': '̈', "'`": '̚', "')": '͗', "'(": '͑', "'B": 'ʱ', "'C": '̰', "'N": 'ⁿ', "'D": '͆', "'=": '̻', "'L": 'ˡ', "'|": '̴', "'X": '̽', "'F": '̊', "'V": '̌', "'~": '̃',
+    '^': '^',
+    '^+': '˖',
+    '^-': '˗',
+    '^%': 'ᵔ',
+    '^;': '˕',
+    '^,': 'ˈ',
+    '^_': '˽',
+    '^H': 'ʰ',
+    '^W': 'ʷ',
+    '^M': '̼',
+    '^J': 'ʲ',
+    '^G': 'ˠ',
+    '^P': 'ˤ',
+    '^!': '˔',
+    '^>': '꭪',
+    '^<': '꭫',
+    '^R': '˞',
+    '^"': '¨',
+    '^`': '˺',
+    '^)': '˒',
+    '^(': '˓',
+    '^B': 'ʱ',
+    '^C': '̰',
+    '^N': 'ⁿ',
+    '^D': '̪',
+    '^=': '▫',
+    '^L': 'ˡ',
+    '^|': '̴',
+    '^X': 'ˣ',
+    '^F': '˳',
+    '^V': 'ˬ',
+    '^~': '˜',
+    '/': '/',
+    '/+': '̟',
+    '/-': '̠',
+    '/%': '̯',
+    '/;': '̞',
+    '/,': '̩',
+    '/_': '̺',
+    '/H': 'ₕ',
+    '/W': 'ʷ',
+    '/M': '̼',
+    '/J': 'ⱼ',
+    '/G': 'ᵧ',
+    '/P': 'ₚ',
+    '/!': '̝',
+    '/>': '̘',
+    '/<': '̙',
+    '/R': '˞',
+    '/"': '̤',
+    '/`': '͉',
+    '/)': '̹',
+    '/(': '̜',
+    '/B': '̤',
+    '/C': '̰',
+    '/N': 'ₙ',
+    '/D': '̪',
+    '/=': '̻',
+    '/L': 'ₗ',
+    '/|': '̴',
+    '/X': 'ₓ',
+    '/F': '̥',
+    '/V': '̬',
+    '/~': '̰',
+    '.': '.',
+    '.+': 'ː',
+    '.-': 'ˑ',
+    '.%': '̆', ".'": 'ˈ',
+    '.,': 'ˌ',
+    '.:': '‖',
+    '.|': '|',
+    '._': '͜',
+    '.=': '͡',
+    '.(': '⁀',
+    '.)': '‿',
+    '\\ ': ' ',
+    '\\!': '!',
+    '\\"': '"',
+    '\\#': '#',
+    '\\$': '$',
+    '\\%': '%',
+    '\\&': '&', "\\'": "\\'", '\\(': '(',
+    '\\)': ')',
+    '\\*': '*',
+    '\\+': '+',
+    '\\,': ',',
+    '\\-': '-',
+    '\\.': '.',
+    '\\/': '/',
+    '\\0': '0',
+    '\\1': '1',
+    '\\2': '2',
+    '\\3': '3',
+    '\\4': '4',
+    '\\5': '5',
+    '\\6': '6',
+    '\\7': '7',
+    '\\8': '8',
+    '\\9': '9',
+    '\\:': ':',
+    '\\;': ';',
+    '\\<': '<',
+    '\\=': '=',
+    '\\>': '>',
+    '\\?': '?',
+    '\\@': '@',
+    '\\A': 'A',
+    '\\B': 'B',
+    '\\C': 'C',
+    '\\D': 'D',
+    '\\E': 'E',
+    '\\F': 'F',
+    '\\G': 'G',
+    '\\H': 'H',
+    '\\I': 'I',
+    '\\J': 'J',
+    '\\K': 'K',
+    '\\L': 'L',
+    '\\M': 'M',
+    '\\N': 'N',
+    '\\O': 'O',
+    '\\P': 'P',
+    '\\Q': 'Q',
+    '\\R': 'R',
+    '\\S': 'S',
+    '\\T': 'T',
+    '\\U': 'U',
+    '\\V': 'V',
+    '\\W': 'W',
+    '\\X': 'X',
+    '\\Y': 'Y',
+    '\\Z': 'Z',
+    '\\[': '[',
+    '\\\\': '\\\\',
+    '\\]': ']',
+    '\\^': '^',
+    '\\_': '_',
+    '\\`': '`',
+    '\\a': 'a',
+    '\\b': 'b',
+    '\\c': 'c',
+    '\\d': 'd',
+    '\\e': 'e',
+    '\\f': 'f',
+    '\\g': 'g',
+    '\\h': 'h',
+    '\\i': 'i',
+    '\\j': 'j',
+    '\\k': 'k',
+    '\\l': 'l',
+    '\\m': 'm',
+    '\\n': 'n',
+    '\\o': 'o',
+    '\\p': 'p',
+    '\\q': 'q',
+    '\\r': 'r',
+    '\\s': 's',
+    '\\t': 't',
+    '\\u': 'u',
+    '\\v': 'v',
+    '\\w': 'w',
+    '\\x': 'x',
+    '\\y': 'y',
+    '\\z': 'z',
+    '\\{': '{',
+    '\\|': '|',
+    '\\}': '}',
+    '\\~': '~',
+    'a': 'a',
+    'a"': 'ɐ',
+    'a)': 'ɶ',
+    'b': 'b',
+    'b<': 'ɓ',
+    'b!': 'ʘ',
+    'bB': 'ʙ',
+    'bH': 'β',
+    'c': 'c',
+    'c<': 'ʄ',
+    'c!': 'ǂ',
+    'cH': 'ç',
+    'cL': '𝼆',
+    'd': 'd',
+    'd<': 'ɗ',
+    'd!': 'ǀ',
+    'd-': 'ɖ',
+    'dH': 'ð',
+    'e': 'e',
+    'e"': 'ə',
+    'e)': 'ø',
+    'f': 'f',
+    'g': 'g',
+    'g<': 'ɠ',
+    'g-': 'ɢ',
+    'gH': 'ɣ',
+    'gQ': '\uf001',
+    'gJ': 'ɟ',
+    'gL': 'ʟ',
+    'h': 'h',
+    'h"': 'ɦ',
+    'hH': 'ʜ',
+    'h+': 'ħ',
+    'i': 'i',
+    'i"': 'ɨ',
+    'j': 'j',
+    'jW': 'ɥ',
+    'jL': 'ʎ',
+    'jH': 'ʝ',
+    'k': 'k',
+    'l': 'l',
+    'l-': 'ɭ',
+    'l`': 'ɺ',
+    'l!': 'ǁ',
+    'lS': 'ʪ',
+    'lZ': 'ʫ',
+    'm': 'm',
+    'm-': 'ɱ',
+    'n': 'n',
+    'n-': 'ɳ',
+    'nJ': 'ɲ',
+    'nG': 'ŋ',
+    'nQ': 'ɴ',
+    'o': 'o',
+    'o"': 'ɵ',
+    'o(': 'ɤ',
+    'p': 'p',
+    'pH': 'ɸ',
+    'q': 'q',
+    'q<': 'ʛ',
+    'qR': 'ʀ',
+    'qQ': '\uf000',
+    'r': 'r',
+    'r-': 'ɻ',
+    'rT': 'ɽ',
+    'r"': 'ɹ',
+    'r|': '𝼊',
+    'rH': 'ʁ',
+    'r`': 'ɾ',
+    'r!': '‼',
+    's': 's',
+    's-': 'ʂ',
+    'sJ': 'ɕ',
+    'sL': 'ɬ',
+    'sH': 'ʃ',
+    't': 't',
+    't!': 'ǃ',
+    't-': 'ʈ',
+    'tH': 'θ',
+    'u': 'u',
+    'u"': 'ʉ',
+    'u(': 'ɯ',
+    'v': 'v',
+    'vW': 'ʋ',
+    'v`': 'ⱱ',
+    'w': 'w',
+    'w(': 'ɰ',
+    'x': 'x',
+    'x-': 'χ',
+    'xW': 'ʍ',
+    'xH': 'ɧ',
+    'xL': '\uf268',
+    'y': 'y',
+    'z': 'z',
+    'zJ': 'ʑ',
+    'zL': 'ɮ',
+    'z-': 'ʐ',
+    'zH': 'ʒ'};
+
+//https://www.w3schools.com/js/js_functions.asp
+function parse(string){
+    //https://www.w3schools.com/js/js_if_else.asp
+    //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/length
+    if (string.length == 0){
+        return string;
+    }
+        
+    var output = "";
+    var last_digraph = false;
+    var i = 0;
+    //https://www.w3schools.com/js/js_loop_while.asp
+    while (i < string.length - 1) {
+        //https://www.w3schools.com/jsref/jsref_substring.asp
+        //https://sentry.io/answers/difference-between-let-and-var-in-javascript/
+        let substr = string.substring(i,i+2);
+        let digraph_result = graphemes[substr];
+        
+        //https://stackoverflow.com/questions/19317943/why-referencing-non-existent-property-of-an-object-in-javascript-doesnt-return
+        if (digraph_result != null) {
+            last_digraph = true;
+            output += digraph_result;
+            i += 2;
+        } else {
+            last_digraph = false;
+            let substr = string.substring(i, i+1);
+            let monograph_result = graphemes[substr];
+            if (monograph_result != null) {
+                output += monograph_result;
+                i += 1;
+            }else {
+                //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals
+                return output + `There was an error at character ${i}`;
+            }
+        }
+    }
+    if (i == string.length - 1) {
+        let substr = string.substring(string.length - 1, string.length);
+        let monograph_result = graphemes[substr];
+        if (monograph_result != null) {
+            output += monograph_result;
+        } else{
+            if (last_digraph == false) {
+                return `There was an error at character ${i}`;
+            }
+        }
+    }
+
+    return output;
+}
+
+function input_to_output() {
+    var converter_form = document.getElementById("ascii_converter");
+    //https://developer.mozilla.org/en-US/docs/Web/API/FormData/get
+    var form_data = new FormData(converter_form);
+
+    var output = parse(form_data.get("tipo_input"));
+
+    var wrapper = form_data.get("wrapper");
+
+    if (wrapper == "apostrophes") {
+        output = "'" + output + "'";
+    } else if (wrapper == "quotes") {
+        output = '"' + output + '"';
+    } else if (wrapper == "slashes") {
+        output = '/' + output + '/';
+    } else if (wrapper == "brackets") {
+        output = '[' + output + ']';
+    }
+
+    //https://stackoverflow.com/questions/52097840/how-to-overwrite-text-in-p-tag-using-javascript
+    document.getElementById("converter_output").innerText = output;
+
+    //https://www.w3schools.com/howto/howto_js_copy_clipboard.asp
+    if (form_data.get("copy") == "yes") {
+        navigator.clipboard.writeText(output);
+    }
+
+    //https://stackoverflow.com/questions/64619241/how-do-you-change-a-html-text-after-form-submission
+    return false;
+}
