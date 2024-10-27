16e2e5e8aebda1d6ad3f6a171f722a403d6b25d8
Ethan Carlson <98480330+ethan-j-carlson@users.noreply.github.com>
2024-10-27 15:27:18 -0400

    Actually fixed the bug this time.

--- product_site/js/simple_script.js
+++ product_site/js/simple_script.js
@@ -496,7 +496,7 @@ function input_to_output() {
     if (!parsing_error) {
         size_str = "The output has <strong>";
         if (size_diff > 0) {
-            size_str += output_bytes + "</strong> more bytes than the input";
+            size_str += size_diff + "</strong> more bytes than the input";
             //https://stackoverflow.com/questions/11832914/how-to-round-to-at-most-2-decimal-places-if-necessary
             size_str += "<em> (" + (output_bytes*100/input_bytes ).toFixed(1);
             size_str += "% bigger)</em>.";
